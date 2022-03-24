3526312b5c46fe00cf620320567334a44fbbfc64
Gabrieljr42 <71909624+Gabrieljr42@users.noreply.github.com>
2022-03-23 21:46:10 -0300

    Código funcional com 8 bits de dados
    Código funcionando caso sempre seja mandado 8 bits de dados

--- TransferenciaDadosTP/src/transferenciadadostp/Receptor.java
+++ TransferenciaDadosTP/src/transferenciadadostp/Receptor.java
@@ -32,50 +32,58 @@ public class Receptor {
     
     private boolean[] decoficarDadoHemming(boolean bits[]){
         //Primeira parte seria verificar se houve perda de dados com o ruido
-        boolean[] bitsCorretos = new boolean[12];
+        boolean bitsCorretos;
+        
+        //Usando XOR para verificação se houve perda de dados
+        //Começando sempre de baixo para cima
         String num="";
-        bitsCorretos[0] = bits[0] ^ bits[2] ^ bits[4] ^ bits[6] ^ bits[8] ^ bits[10];
-        if( bitsCorretos[0]){
+          bitsCorretos = bits[7] ^ bits[8] ^ bits[9] ^ bits[10] ^ bits[11];
+          if( bitsCorretos){
             num += 1;
         }else{
             num += 0;
         }
-        bitsCorretos[1] = bits[1] ^bits[2] ^ bits[5] ^ bits[6] ^ bits[9] ^ bits[10];
-        if( bitsCorretos[1]){
+         bitsCorretos= bits[3] ^bits[4] ^ bits[5] ^ bits[6] ^ bits[11];
+          if( bitsCorretos){
             num += 1;
         }else{
             num += 0;
         }
-        bitsCorretos[3] = bits[3] ^bits[4] ^ bits[5] ^ bits[6] ^ bits[11];
-          if( bitsCorretos[3]){
+        bitsCorretos = bits[1] ^bits[2] ^ bits[5] ^ bits[6] ^ bits[9] ^ bits[10];
+        if( bitsCorretos){
             num += 1;
         }else{
             num += 0;
         }
-        bitsCorretos[7] = bits[7] ^bits[8] ^ bits[9] ^ bits[10] ^ bits[11];
-          if( bitsCorretos[7]){
+        bitsCorretos = bits[0] ^ bits[2] ^ bits[4] ^ bits[6] ^ bits[8] ^ bits[10];
+        if( bitsCorretos){
             num += 1;
         }else{
             num += 0;
         }
-        int resul=0;
-        System.out.println(num);
-        for(int x = 0; x!= num.length();x++){
-            if(num.substring(x,x+1)== "1"){
-                resul += Math.pow(2, Integer.parseInt(num.substring(x,x+1)));
-            }
+           
+        //Transformando a casa de binario para decimal usando ParseInt
+        int erro = Integer.parseInt(num, 2);
+           
+        //Se for 0 não houve perda de dados, se não for o número resposta é a casa onde houve o ruido e precisa ser corrigido
+        if(erro != 0){
+            bits[erro-1] = !bits[erro-1];
         }
-        if(resul != 0){
-            bits[resul] = !bits[resul];
-        }
+        //Variavel auxiliar usada para colocar os 12 bits de Hamming para 8 bits de dado
         int intAdicional=0;
+        
+        //Bits que serão enviados para serem decodificados
         boolean newBits[] = new boolean[8];
+        
+        //Colocando os 8 bits de dados em um vetor somente com eles, para decodificação
          for(int x = 0; x!= bits.length; x++){
-            if(x != 0 && x != 1 && x != 4 && x != 8){
+            if(x != 0 && x != 1 && x != 3 && x != 7){
                 newBits[intAdicional] = bits[x];
                 intAdicional++;
             }   
         }
+         
+         //Retornando os novos bits se houve perda de dados
         return newBits;
         
     }
@@ -84,8 +92,8 @@ public class Receptor {
     //recebe os dados do transmissor
     public void receberDadoBits(boolean bits[]){
         
-        //aqui você deve trocar o médodo decofificarDado para decoficarDadoHemming (implemente!!)
         bits = decoficarDadoHemming(bits);
+        
         decodificarDado(bits);
     }
 }
--- TransferenciaDadosTP/src/transferenciadadostp/Transmissor.java
+++ TransferenciaDadosTP/src/transferenciadadostp/Transmissor.java
@@ -43,27 +43,30 @@ public class Transmissor {
     }
     
     private boolean[] dadoBitsHemming(boolean bits[]){
+        //Criando o vetor com 12 casas, 8 com dados e 4 com o código de Hamming
         boolean bitsHemming[] = new boolean[12];
-        int bitsAdicionais = 4, intAdicional=0;
+        
+       //Variavel auxiliar usada para colocar os 8 bits de dado para 12 bits de Hamming
+        int intAdicional=0;
+        
         for(int x = 0; x!= bitsHemming.length; x++){
-            if(x != 0 && x != 1 && x != 4 && x != 8){
-                //bitsHemming[x] = true;
+            //Como já sabemos as casas que serão usadas para o código, já separamos elas( tirando -1 devido o vetor começar em 0)
+            if(x != 0 && x != 1 && x != 3 && x != 7){
                 bitsHemming[x] = bits[intAdicional];
                 intAdicional++;
             }   
         }
-        //Setando os números nas casas;
-        // Para a casa 0 :  3 -> 5 -> 7 -> 9 -> 11
-        // Para a casa 2 :  3 -> 6 -> 7 ->10 -> 11
-        // Para a casa 4 :  5 -> 6 -> 7 -> 12 
-        // Para a casa 8 :  9 -> 10 -> 11 -> 12
+        
+       //Fazendo o XOR para achar os bits de Hamming(Sempre lembranco que começa no 0 e vai até o 11)
+       //0 -> 2,4,6,8,10
+       //1 -> 2,5,6,9,10
+       //3 -> 4,5,6,11
+       //7 -> 8,9,10,11
         bitsHemming[0] = bitsHemming[2] ^ bitsHemming[4] ^ bitsHemming[6] ^ bitsHemming[8] ^ bitsHemming[10];
         bitsHemming[1] = bitsHemming[2] ^ bitsHemming[5] ^ bitsHemming[6] ^ bitsHemming[9] ^ bitsHemming[10];
         bitsHemming[3] = bitsHemming[4] ^ bitsHemming[5] ^ bitsHemming[6] ^ bitsHemming[11];
         bitsHemming[7] = bitsHemming[8] ^ bitsHemming[9] ^ bitsHemming[10] ^ bitsHemming[11];
-        for(int x = 0; x!= bitsHemming.length;x++){
-          
-        }
+      
         return bitsHemming;
     }
     
@@ -71,7 +74,7 @@ public class Transmissor {
         for(int i = 0; i < this.mensagem.length();i++){
             boolean bits[] = streamCaracter(this.mensagem.charAt(i));
             
-          
+            //Transformando os dados de bits normais para bitsDeHamming.
             boolean bitsHemming[] = dadoBitsHemming(bits);
             
             
